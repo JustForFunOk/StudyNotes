f1fb447591bcf8fa64dccda707cdd2b5a05c8144
SJ <slgsunjian@163.com>
2021-01-04 21:45:32 +0800

    extern extand

--- cxx/projects/extern_c/main.cpp
+++ cxx/projects/extern_c/main.cpp
@@ -1,7 +1,22 @@
-#include "c_lib.h"
+#include "c_lib.h"  // 
+#include <stdio.h>
+
+// same function name, the symbol name is sumTwoInt, and use this function not the function in .so
+int sumTwoInt(int a, int b)
+{
+    return a-b;
+}
+
+float sumTwoFloat(float a, float b)
+{
+    return a+b;
+}
 
 int main()
 {
     int sum = sumTwoInt(1, 2);
+    printf("sum = %d\n", sum);
+    float sum_float = sumTwoFloat(1.0, 2.0);
+    printf("sum_float = %f\n", sum_float);
     return 0;
 }
--- cxx/projects/extern_c/main.s
+++ cxx/projects/extern_c/main.s
@@ -1,9 +1,53 @@
 	.file	"main.cpp"
 	.text
+	.globl	sumTwoInt
+	.type	sumTwoInt, @function
+sumTwoInt:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %eax
+	subl	-8(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sumTwoInt, .-sumTwoInt
+	.globl	_Z11sumTwoFloatff
+	.type	_Z11sumTwoFloatff, @function
+_Z11sumTwoFloatff:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movss	%xmm0, -4(%rbp)
+	movss	%xmm1, -8(%rbp)
+	movss	-4(%rbp), %xmm0
+	addss	-8(%rbp), %xmm0
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	_Z11sumTwoFloatff, .-_Z11sumTwoFloatff
+	.section	.rodata
+.LC0:
+	.string	"sum = %d\n"
+	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB2:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -14,13 +58,30 @@ main:
 	movl	$2, %esi
 	movl	$1, %edi
 	call	sumTwoInt
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movss	.LC1(%rip), %xmm1
+	movss	.LC2(%rip), %xmm0
+	call	_Z11sumTwoFloatff
+	movd	%xmm0, %eax
 	movl	%eax, -4(%rbp)
 	movl	$0, %eax
 	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE2:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.rodata
+	.align 4
+.LC1:
+	.long	1073741824
+	.align 4
+.LC2:
+	.long	1065353216
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",@progbits
