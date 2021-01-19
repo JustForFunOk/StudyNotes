d016390e8a6b66896cc67c3a2fafcd7fe5e8f1df
JustForFun <slgsunjian@163.com>
2021-01-19 16:11:58 +0800

    finish merge sort

--- cxx/projects/sort/my_merge_sort.cpp
+++ cxx/projects/sort/my_merge_sort.cpp
@@ -4,65 +4,63 @@
 
 using namespace std;
 
-int partition(vector<int>& vec, int left, int right)
-{
-    int pivot = vec[left];  // choose left element as pivot
-    bool right_handle = true;  // start from right
+vector<int> tmp;  // 需要额外的空间来临时储存排序过程中顺序
 
-    while(left < right)
-    {
-        if(right_handle)
-        {
-            if(vec[right] > pivot)
-            {
-                right--;
-            }
-            else
-            {
-                vec[left] = vec[right];
-                right_handle = false;
-                left++;
-            }
-        }
-        else
-        {
-            if(vec[left] < pivot)
-            {
-                left++;
-            }
-            else
-            {
-                vec[right] = vec[left];
-                right_handle = true;
-                right--;
-            }
-        }
-    }
-
-    vec[left] = pivot;
-
-    return left;
-}
-
-void quick_sort(vector<int>& vec, int left, int right)
+void merge_sort(vector<int>& vec, int left, int right)
 {
     if(left >= right)
     {
         return;
     }
 
-    // partition
-    int pivot_idx = partition(vec, left, right);
+    // 归
+    int mid = left + (right - left) / 2;
+    merge_sort(vec, left, mid);
+    merge_sort(vec, mid+1, right);
 
-    quick_sort(vec, left, pivot_idx-1);
-    quick_sort(vec, pivot_idx+1, right);
+    // 并
+    // 合并两个有序数组(双指针)
+    int i = left;  // 指针1
+    int j = mid+1; // 指针2
+    int cnt = 0;
+
+    while(i<=mid && j<=right)
+    {
+        if(vec[i] < vec[j])
+        {
+            tmp[cnt++] = vec[i++];
+        }
+        else
+        {
+            tmp[cnt++] = vec[j++];
+        }
+    }
+
+    while(i <= mid)
+    {
+        tmp[cnt++] = vec[i++];
+    }
+
+    while(j <= right)
+    {
+        tmp[cnt++] =  vec[j++];
+    }
+
+    for(int i = 0; i < right-left+1; ++i)
+    {
+        vec[i+left] = tmp[i];
+    }
+
+    return;
 }
 
 int main()
 {
     vector<int> vec = {4, 6, 7, 2, 8, 9, 2, 9, 14, 54};
 
-    quick_sort(vec, 0, vec.size()-1);
+    tmp.resize(vec.size(), 0); // 初始化为与待排数组同样的尺寸
+
+    merge_sort(vec, 0, vec.size()-1);
 
     for_each(vec.begin(), vec.end(), [&vec](int ele){cout << ele << " ";});
 
